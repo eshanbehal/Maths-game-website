36af0b2ead8224316edf468b40692c07d7499d6f
eshan786 <eshan786behal@gmail.com>
2019-09-21 13:35:14 +0530

    added basicc concept behind game.

---
+++ javascript.js
@@ -0,0 +1,23 @@
+//if we click on start/reset button
+     //if we are playing
+          //reload page
+     //if we are not playing
+          //set score to 0.
+          //show countdown box.
+          //reduce time by 1 sec in loops.
+               //timeleft?
+                    //yes->continue.
+                    //no->gameover.
+           //change button to reset.
+           //generate new q&a.
+
+
+//if we click on ans box
+       //if we are playing
+            //correct?
+               //yes
+                   //increase score
+                   //show correct box for 1 sec.
+                   //generate new q&a.
+                //no
+                    //show try again box for 1 sec.
