726f9c1786b309031aea08c964e5b3614f7957d2
eshan786 <eshan786behal@gmail.com>
2019-09-22 09:33:02 +0530

    added functionality to my code.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
-//if we click on start/reset button
-     //if we are playing
-          //reload page
-     //if we are not playing
-          //set score to 0.
-          //show countdown box.
+
+     
+          
+     
+          
+          
           //reduce time by 1 sec in loops.
                //timeleft?
                     //yes->continue.
                     //no->gameover.
-           //change button to reset.
+           
            //generate new q&a.
 
 
